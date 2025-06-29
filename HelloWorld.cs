da15a3f88ae82ba323903b62d609bbffa6ac181b
dunya212 <danilkaplayyt1@gmail.com>
2025-06-29 15:50:46 +0700

    Комита

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //вывод в консоль приветствие к миру
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
